2a09f3541de480b0191de15f69e94319382ec4e8
jardel <jardel_96_2010@hotmail.com>
2019-08-15 11:02:13 -0300

    teste 3 deplotment continuos

---
+++ ve.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+	<html lang ="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>primeiro exemplo</title>
+
+	</head>
+	<body>
+	<div>		<form class="form" action="veraula.php" method="POST">
+
+				<div class="containerP">
+					<p>Escolha a Discilpina: <span></span></p>
+					<select name="ingresso" >
+						
+						<option value="1" selected> Pintura Digital</option>
+						<option value="2"> Animação Classica</option>
+						<option value="6">Fundamento 3D</option>
+						<option value="7">Linguagem Audio Visual</option>
+						<option value="8">Desenho</option>
+						<input type="submit" name="enviar">
+						
+					</select>
+				</div>
+			</form>
+
+			<form class="form" action="forviraaula.php" method="POST">
+
+				<div class="containerP">
+					<p>ir para virada de aula: <span></span></p>
+					<input type="submit" name="IR">
+						
+						 
+						
+					
+				</div>
+
+
+
+				</div>
+			</body>
+			</html>
+			
+
+
